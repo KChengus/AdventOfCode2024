e97c9babb8688dc181484c4fddb51b38b5b44f36
KChengus <02chengkevin@gmail.com>
2024-12-01 16:06:18 +0100

    day1 done

--- Day1/Day1p1.py
+++ Day1/Day1p1.py
@@ -2,18 +2,29 @@ import re
 
 class Solution():
     def __init__(self):
-        pass
+        self.content = list()
 
     def solve(self):
-        pass
+        left, right = [], []
+        for line in self.content:
+            left.append(int(line[0]))
+            right.append(int(line[1]))
+        left.sort()
+        right.sort()
+        ret = 0
+        for i, j in zip(left, right):
+            ret += abs(i - j)
+        return ret
 
     def handle_input(self):
-        pass
-
+        with open('input.txt', 'r') as f:
+            for line in f:
+                self.content.append(line.split())
+                
     def run(self):
-        pass
-
-    
+        self.handle_input()
+        return self.solve()
+       
 
 if __name__ == "__main__":
     sol = Solution()
--- Day1/Day1p2.py
+++ Day1/Day1p2.py
@@ -2,16 +2,30 @@ import re
 
 class Solution():
     def __init__(self):
-        pass
+        self.content = list()
 
     def solve(self):
-        pass
+        left, right = [], {}
+        for line in self.content:
+            left.append(int(line[0]))
+            r = int(line[1])
+            if r not in right:
+                right[r] = 0
+            right[r] += 1
 
+        ret = 0
+        for num in left:
+            if num in right:
+                ret += num * right[num]
+        return ret
     def handle_input(self):
-        pass
+        with open('input.txt', 'r') as f:
+            for line in f:
+                self.content.append(line.split())
 
     def run(self):
-        pass
+        self.handle_input()
+        return self.solve()
 
     
 
